017a47a3c98a33f1311d2224191a7dc4b70ffdb5
Linker <89202168+G-DLion@users.noreply.github.com>
2023-05-07 19:10:44 +0300

    модифицировал код

--- getUrl.py
+++ getUrl.py
@@ -12,17 +12,21 @@ bkex_data = {}
 okx_data = {}
 kucoin_data = {}
 
+page_exchange = { # api для запросов
+        'Binance': 'https://api.binance.com/api/v3/ticker/price?symbol=',
+        'Bybit': 'https://api.bybit.com/v5/market/tickers?category=inverse&symbol=',
+        'Huobi': 'https://api.huobi.pro/market/detail/merged?symbol=',
+        'OKX': 'https://aws.okx.com/api/v5/market/index-tickers?instId=',
+        'KuCoin': 'http://api.kucoin.com/api/v1/market/orderbook/level1?symbol=',
+        'BKEX': 'https://api.bkex.com/v2/q/tickers?symbol=',
+    }
+
+list_crypto = ['BTC', 'ETH'] # валюта
 
 def get_data(mainExchange, baseActive, balance):
-    list_crypto = ['BTC', 'ETH'] #в этом списке будет храниться вся крипта, но пока только 2
-    page_exchange = {
-        'Binance': 'https://api.binance.com/api/v3/ticker/price?symbol=', 
-        'Bybit': 'https://api.bybit.com/v5/market/tickers?category=inverse&symbol=', 
-        'Huobi': 'https://api.huobi.pro/market/detail/merged?symbol=', 
-        'OKX': 'https://aws.okx.com/api/v5/market/index-tickers?instId=',
-        'KuCoin': 'http://api.kucoin.com/api/v1/market/orderbook/level1?symbol=', 
-        'BKEX': 'https://api.bkex.com/v2/q/tickers?symbol=',
-    } #ссылки на api запросы
+    mainExchange = mainExchange.lower()
+
+    resultArray = []
 
     for sym in list_crypto:
         for name_exchange, url in page_exchange.items():
@@ -31,11 +35,13 @@ def get_data(mainExchange, baseActive, balance):
                 huobi_data['name'] = name_exchange
                 huobi_data['price'] = price
                 huobi_data['symbol'] = sym + baseActive
-                if mainExchange == 'Huobi':
+                if 'huobi' in mainExchange:
                     huobi_data['attrib'] = True
+                    resultArray.append(huobi_data)
                 else:
                     huobi_data['attrib'] = False
-                print(huobi_data)
+
+
             elif name_exchange == 'BKEX': #получение данных с BKEX
                 responses = requests.get(url + sym + '_' + baseActive).json()
                 if name_exchange == 'BKEX':
@@ -43,11 +49,13 @@ def get_data(mainExchange, baseActive, balance):
                     bkex_data['name'] = name_exchange
                     bkex_data['price'] = price
                     bkex_data['symbol'] = sym + baseActive
-                    if mainExchange == 'BKEX':
+                    if 'bkex' in mainExchange:
                         bkex_data['attrib'] = True
+                        resultArray.append(bkex_data)
                     else:
                         bkex_data['attrib'] = False
-                    print(bkex_data)
+
+
             elif name_exchange == 'OKX' or name_exchange == 'KuCoin': #получение данных с Huobi или c KuCoin
                 responses = requests.get(url + sym + '-' + baseActive).json()
                 if name_exchange == 'OKX':
@@ -55,21 +63,24 @@ def get_data(mainExchange, baseActive, balance):
                     okx_data['name'] = name_exchange
                     okx_data['price'] = price
                     okx_data['symbol'] = sym + baseActive
-                    if mainExchange == 'OKX':
+                    if 'okx' in mainExchange:
                         okx_data['attrib'] = True
+                        resultArray.append(okx_data)
                     else:
                         okx_data['attrib'] = False
-                    print(okx_data)
+
+
                 elif name_exchange == 'KuCoin':
                     price = responses['data']['price']
                     kucoin_data['name'] = name_exchange
                     kucoin_data['price'] = price
                     kucoin_data['symbol'] = sym + baseActive
-                    if mainExchange == 'KuCoin':
+                    if 'kucoin' in mainExchange:
                         kucoin_data['attrib'] = True
+
                     else:
                         kucoin_data['attrib'] = False
-                    print(kucoin_data)
+
             else: #получение данных с Binance или с Bybit
                 responses = requests.get(url + sym + baseActive).json()
                 if name_exchange == 'Binance':
@@ -77,30 +88,37 @@ def get_data(mainExchange, baseActive, balance):
                     binance_date['name'] = name_exchange
                     binance_date['price'] = price
                     binance_date['symbol'] = sym + baseActive
-                    if mainExchange == 'Binance':
+                    if 'binance' in mainExchange:
                         binance_date['attrib'] = True
+
                     else:
                         binance_date['attrib'] = False
-                    print(binance_date)
+
 
                 elif name_exchange == 'Bybit':
                     price = responses['result']['list'][0]['lastPrice']
                     bybit_date['name'] = name_exchange
                     bybit_date['price'] = price
                     bybit_date['symbol'] = sym + baseActive
-                    if mainExchange == 'Bybit':
+                    if 'bybit' in mainExchange:
                         bybit_date['attrib'] = True
+
                     else:
                         bybit_date['attrib'] = False
-                    print(bybit_date)
+
                 
+    return resultArray
+
 
 '''def test_get_data(): #метод для тестовых запросов
     url = 'https://api.bkex.com/v2/q/tickers?symbol=BTC_USDT'
     req = requests.get(url).json()
     print(req['data'][0]['close'])'''
 
+
 l = ['Binance', 'Huobi', 'Bybit', 'OKX', 'KuCoin', 'BKEX'] #список бирж
 print('Cписок бирж - ', l) 
 mainExchange = input("Введи название биржи: ")
-get_data(mainExchange, 'USDT', 100) #1 - главвная биржа, где храняться наши активы. 2 - базовый актив (пока будет USDT) 3 - кол-во монет (добавлю в будущем)
+test = get_data(mainExchange, 'USDT', 100) #1 - главвная биржа, где храняться наши активы. 2 - базовый актив (пока будет USDT) 3 - кол-во монет (добавлю в будущем)
+print("____")
+print(test)
