0e0bda3e16cd221edd885450a90405b5d3ee51a3
Linker <89202168+G-DLion@users.noreply.github.com>
2023-05-08 23:57:29 +0300

    доработана сортировка 0.01

--- SORT.py
+++ SORT.py
@@ -9,11 +9,16 @@ def ssort(x):
 
 def unpack(data):
     price_data = []
+    name_data = []
     data.sort(key=lambda x: str(x['price']))
-    print(data)
+
     for exchange in data:
         for key, value in exchange.items():
             if key == 'price':
                 price_data.append(float(value))
+            elif key == 'name':
+                name_data.append(str(value))
     print(price_data)
+    print(name_data)
+
 unpack(exchanges)
